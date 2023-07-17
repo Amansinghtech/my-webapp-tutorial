fa50b4a8f734b9d85999a323396dacb660cf995c
Aman Singh <singhaman11415@gmail.com>
2023-07-17 15:30:22 +0530

    completed basic example

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,3 +1,41 @@
+import { Button, Stack, Typography } from '@mui/material'
+import { useState } from 'react'
+
 export default function Home() {
-	return <h1>Hello World</h1>
+	const [counter, setCounter] = useState(0)
+
+	const increment = () => {
+		setCounter(counter + 1)
+	}
+	const decrement = () => {
+		setCounter(counter - 1)
+	}
+
+	return (
+		<Stack direction={'row'} spacing={2} alignItems={'center'}>
+			<Typography variant="h3">Counter Value : {counter}</Typography>
+			<Button
+				variant="contained"
+				sx={{
+					backgroundColor: 'green',
+					height: '40px',
+					width: '150px',
+				}}
+				onClick={increment}
+			>
+				Increment
+			</Button>
+			<Button
+				variant="contained"
+				sx={{
+					backgroundColor: 'green',
+					height: '40px',
+					width: '150px',
+				}}
+				onClick={decrement}
+			>
+				Decrement
+			</Button>
+		</Stack>
+	)
 }
